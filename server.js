91f2ca0040a3540400ee1f353083f23c31c05db7
Aleks Pimen <pimenaleks04@gmail.com>
2024-02-25 19:35:46 -0500

    routes for handling CRUD operations

--- server.js
+++ server.js
@@ -18,7 +18,22 @@ app.get('/', (req, res) => {
     res.send('Dress Store');
   });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const express = require('express');
+const router = express.Router();
+const controller = require('./controller');
+
+router.get('/api/products', controller.getAllProducts);
+router.get('/api/products/:id', controller.getProductById);
+router.post('/api/products', controller.addNewProduct);
+router.put('/api/products/:id', controller.updateProductById);
+router.delete('/api/products/:id', controller.deleteProductById);
+router.delete('/api/products', controller.deleteAllProducts);
+router.get('/api/products', controller.findProductsByName);
+
+module.exports = router;
